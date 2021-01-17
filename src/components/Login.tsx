e282bf8d7b874f8c995a7ce58c3d632a396320a9
Luis Suarez <suarezluis@gmail.com>
2021-01-17 17:37:44 -0600

    Add front page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Collage from "../assets/images/collage.png";
 
 const Login = () => {
-  return <LoginWrapper>Please log in to use this app</LoginWrapper>;
+  return (
+    <LoginWrapper>
+      <Title>
+        Lets make all your images <br /> the same aspect ratio.
+      </Title>
+      <StyledImage src={Collage} alt="" />
+    </LoginWrapper>
+  );
 };
 
 export default Login;
@@ -11,6 +19,18 @@ const LoginWrapper = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const Title = styled.div`
+  font-size: 40px;
+  font-weight: 900;
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
+const StyledImage = styled.img`
+  width: 75%;
 `;
